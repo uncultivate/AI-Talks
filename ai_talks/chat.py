5f4998392fa0e255bc66e6b6cc9ad6715cffd647
Jonathan Sheahan <jon.sheahan@gmail.com>
2023-09-21 10:54:42 +1000

    Update chat.py

--- ai_talks/chat.py
+++ ai_talks/chat.py
@@ -9,7 +9,7 @@ from streamlit_option_menu import option_menu
 import numpy as np
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 import random
 import math
 import time
