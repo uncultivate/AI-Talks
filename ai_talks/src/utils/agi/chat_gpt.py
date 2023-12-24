c905eb5ad224bf790409b4239530a1166c399e5c
Jonathan Sheahan <jon.sheahan@gmail.com>
2023-12-24 16:20:36 +1100

    Update chat_gpt.py

--- ai_talks/src/utils/agi/chat_gpt.py
+++ ai_talks/src/utils/agi/chat_gpt.py
@@ -1,7 +1,7 @@
 import logging
 from typing import List  # NOQA: UP035
 
-from openai import OpenAI_API_Client, OpenAI_Error
+from openai import OpenAI
 import streamlit as st
 client = OpenAI(api_key=st.secrets.api_credentials.api_key)
 #client = OpenAI(
@@ -13,18 +13,13 @@ client = OpenAI(api_key=st.secrets.api_credentials.api_key)
 
 @st.cache_data()
 def loading_data(ai_model: str, messages: List[dict]) -> dict:
-    try:
-            # Check if image processing is required
-            model = ai_model
 
-            completion = client.chat_completions.create(model=model, messages=messages)
+    logging.info(f"{messages=}")
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model="gpt-3.5-turbo",
+    )
+    logging.info(f"{chat_completion=}")
+    return chat_completion
 
-        except (KeyError, AttributeError, OpenAI_Error) as error:
-            # Handle specific errors (e.g., API errors, attribute errors)
-            logging.error(f"Error loading data: {error}")
-            raise
-
-        logging.info(f"Loaded data with {messages=}")
-        logging.info(f"{completion=}")
-
-        return completion
+    
