aad7211a9d4e4f47419a1d50e2ce1a423f67368f
Jonathan Sheahan <jon.sheahan@gmail.com>
2023-12-27 11:33:08 +1100

    Update conversation.py

--- ai_talks/src/utils/conversation.py
+++ ai_talks/src/utils/conversation.py
@@ -42,36 +42,43 @@ def show_chat2(ai_content: str) -> None:
         # store the ai content
         st.session_state.news.append(ai_content)
 
+def extract_message_content(chat_completion):
+    messages = []
+    for choice in chat_completion.choices:
+        messages.append(choice.message.content)
+    return messages
+
 
 def show_gpt_conversation(bm) -> None:
 
     completion = loading_data(st.session_state.model, st.session_state[bm])
-    ai_content = completion.get("choices")[0].get("message").get("content")
+    extracted_messages = extract_message_content(completion)
     #calc_cost(completion.get("usage"))
-    st.session_state[bm].append({"role": "assistant", "content": ai_content})
-    if ai_content:
-        show_chat(ai_content)
+    st.session_state[bm].append({"role": "assistant", "content": extracted_messages})
+    if extracted_messages:
+        show_chat(extracted_messages)
+   
    
 
 def show_gpt_conversation2() -> None:
 
     completion = loading_data(st.session_state.model, st.session_state.messages)
-    ai_content = completion.get("choices")[0].get("message").get("content")
+    extracted_messages = extract_message_content(completion)
     #calc_cost(completion.get("usage"))
-    st.session_state.messages.append({"role": "assistant", "content": ai_content})
-    if ai_content:
-        show_chat(ai_content)
+    #st.session_state.messages.append({"role": "assistant", "content": ai_content})
+    if extracted_messages:
+        show_chat(extracted_messages)
    
 
 def show_gpt_conversation3() -> None:
 
     completion = loading_data(st.session_state.model, st.session_state.messages)
     st.write(completion)
-    ai_content = completion.get("choices")[0].get("message").get("content")
+    extracted_messages = completion.get("choices")[0].get("message").get("content")
     #calc_cost(completion.get("usage"))
-    st.session_state.messages.append({"role": "assistant", "content": ai_content})
-    if ai_content:
-        show_chat2(ai_content)
+    #st.session_state.messages.append({"role": "assistant", "content": ai_content})
+    if extracted_messages:
+        show_chat(extracted_messages)
 
 
 
@@ -89,10 +96,6 @@ def show_conversation(ai_role, economic_data, bm) -> None:
         ]
     show_gpt_conversation(bm)
 
-def show_conversation2(ai_role) -> None:
-    st.session_state.messages.append({"role": "user", "content": f'You strongly disagree with the previous advice and refute where possible. Choose different economic metrics to focus on in your response. {ai_role}'})
-    
-    show_gpt_conversation2()
 
 def write_news(ai_role, economic_data, decision=None) -> None:
     if st.session_state.messages:
